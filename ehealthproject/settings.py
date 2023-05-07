c9a627c8a1d0f30a28e66d63d9aff117b6fb40c1
Isah Jacob <isahjacob0@gmail.com>
2023-05-07 15:02:31 +0100

    removed duplicated database

--- ehealthproject/settings.py
+++ ehealthproject/settings.py
@@ -27,17 +27,6 @@ new_secret = ''.join(secrets.choice(chars) for i in range(50))
 SECRET_KEY = 'new_secret'
 
 DEBUG = False
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env("DB_NAME"),
-        "USER": env("DB_USER"),
-        "PASSWORD": env("DB_PASSWORD"),
-        "HOST": env("DB_HOST"),
-        "PORT": env("DB_PORT"),
-    }
-}
 # BASE_DIR = Path(__file__).resolve().parent.parent
 # env = environ.Env()
 # environ.Env.read_env(BASE_DIR / ".env")
